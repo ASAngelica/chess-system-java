3f9b09bf218180f97add43c0d37a878db77e830f
Antonio S Angelica <asa196549@gmail.com>
2020-10-14 17:53:48 -0300

    ChessException and ChessPosition

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -4,6 +4,7 @@ public class Piece {
 	
 	protected Position position;
 	private Board board;
+	
 	public Piece(Board board) {
 		this.board = board;
 		position = null; //O Java já considera null mas foi colocado só para enfatizar didaticamente
